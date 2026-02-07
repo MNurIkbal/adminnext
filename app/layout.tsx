eebeee9f3c023a05cde0b56114fa76f7b76f368f
MNurIkbal <ikbalmnur17@gmail.com>
2026-02-07 13:01:06 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import '../public/assets/css/bootstrap.min.css'
+import '../public/assets/css/apexcharts.css'
+import '../public/assets/css/dataTables.min.css'
+import '../public/assets/css/slick.css'
+import '../public/assets/css/full-calendar.css'
+import '../public/assets/css/style.css'
+import '../public/assets/css/prism.css'
+import '../public/assets/css/jquery-jvectormap-2.0.5.css'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
